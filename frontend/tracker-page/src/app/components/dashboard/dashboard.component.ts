6491e0347c00d4472b385a73ad2f4552fda77419
Andrei Paun <andrei.paun1@s.unibuc.ro>
2023-11-12 03:44:34 +0200

    Added dashboard-account jumping

--- frontend/tracker-page/src/app/components/dashboard/dashboard.component.ts
+++ frontend/tracker-page/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  constructor(private router: Router) { }
+
+  picture: string = 'https://i.pravatar.cc/300';
+
+
+
+  goToAccount() {
+    this.router.navigate(['/account']);
+  }
 
 }
